79189ef3447d3c16706cc108e8233995a262ffdf
abdul <abdul.hamid@tlab.co.id>
2015-12-30 22:13:45 +0700

    edit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,13 +16,16 @@ class UsersTableSeeder extends Seeder
             'group_name' => 'Admin',
         ]);
 
+       \DB::statement("SELECT pg_catalog.setval(pg_get_serial_sequence('groups', 'group_id'), "
+            . "MAX(group_id)) FROM groups");
+
         DB::table('users')->insert([
             'id' => Uuid::generate(),
             'group_id' => '1',
             'username' => 'admin',
             'email' => 'admin@spam4.me',
             'password' => bcrypt('123456'),
-            'name' => 'Admin Tlab',
+            'name' => 'Admin Sistem Inventory',
             'active' => '1',
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now()
