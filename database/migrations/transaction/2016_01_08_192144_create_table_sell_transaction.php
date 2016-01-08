2c1e595a11f4893fad3053c17cf041f455508b5e
abdul <abdul.hamid@tlab.co.id>
2016-01-08 21:03:13 +0700

    edit migrate transaction

--- database/migrations/transaction/2016_01_08_192144_create_table_sell_transaction.php
+++ database/migrations/transaction/2016_01_08_192144_create_table_sell_transaction.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTableSellTransaction extends Migration
 {
+    protected $table = "transaction_sell";
     /**
      * Run the migrations.
      *
@@ -12,7 +13,29 @@ class CreateTableSellTransaction extends Migration
      */
     public function up()
     {
-        //
+      if (!Schema::hasTable($this->table)) {
+
+          Schema::create($this->table, function (Blueprint $table) {
+
+              /** Primary key  */
+              $table->increments('transaction_sell_id');
+
+              /** Main data  */
+              $table->integer('qty');
+              $table->decimal('price_sell',10,2);
+              $table->text('expedition')->nullable();
+
+              /* Action */
+              $table->nullableTimestamps();
+
+              /* Relastio */
+              $table->integer('item_id')->unsigned();
+
+              $table->foreign('item_id')->references('id')
+                ->on('items')->onDelete('cascade')
+                ->onUpdate('cascade');
+          });
+      }
     }
 
     /**
@@ -22,6 +45,6 @@ class CreateTableSellTransaction extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists($this->table);
     }
 }
--- database/migrations/transaction/2016_01_08_192149_create_table_return_transaction.php
+++ database/migrations/transaction/2016_01_08_192149_create_table_return_transaction.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTableReturnTransaction extends Migration
 {
+    protected $table = "transaction_return";
     /**
      * Run the migrations.
      *
@@ -12,7 +13,33 @@ class CreateTableReturnTransaction extends Migration
      */
     public function up()
     {
-        //
+      if (!Schema::hasTable($this->table)) {
+
+          Schema::create($this->table, function (Blueprint $table) {
+
+              /** Primary key  */
+              $table->increments('transaction_return_id');
+
+              /** Main data  */
+              $table->integer('qty');
+              $table->decimal('cost_return',10,2);
+              $table->text('description');
+
+              /* Action */
+              $table->nullableTimestamps();
+
+              /* Relastio */
+              $table->integer('item_id')->unsigned();
+              $table->foreign('item_id')->references('id')
+                ->on('items')->onDelete('cascade')
+                ->onUpdate('cascade');
+
+              $table->integer('transaction_buy_id')->unsigned();
+              $table->foreign('transaction_buy_id')->references('transaction_buy_id')
+                ->on('transaction_buy')->onDelete('cascade')
+                ->onUpdate('cascade');
+          });
+      }
     }
 
     /**
@@ -22,6 +49,6 @@ class CreateTableReturnTransaction extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists($this->table);
     }
 }
